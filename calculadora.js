a807d8b9ec162f696c6f2f8af95cdbd4e8334a2f
Bruna Rodrigues <brunaaheloisa07@gmail.com>
2024-04-03 20:43:29 -0300

    pequenos ajustes

--- calculadora.js
+++ calculadora.js
@@ -1,40 +1,4 @@
 
-/*function calcularPagamento() {
-    // Recupera os valores dos campos do formulário
-    valorConta = parseFloat(document.getElementById("valor").value);
-    numeroPessoas = parseInt(document.getElementById("pessoas").value);
-    metodo = parseInt(document.getElementById("metodo").value);
-
-    // Executa a lógica JavaScript original com base nos valores do formulário
-    pagamentoCliente = valorConta / numeroPessoas;
-    desconto = (pagamentoCliente / 100) * 10;
-
-
-function calcularPagamento() {
-    
-    let valorConta = parseFloat(document.getElementById("valor").value);
-    let numeroPessoas = parseInt(document.getElementById("pessoas").value);
-    let metodo = parseInt(document.getElementById("metodo").value);
-
-    let pagamentoCliente = valorConta / numeroPessoas;
-    let desconto = (pagamentoCliente / 100) * 10;
->>>>>>> 78f8b362c563834279a4da6995cd994078200145
-
-    while(true){
-        console.log("Método de pagamento:");
-        if(metodo === 1) {
-            let pagamento = pagamentoCliente - desconto;
-            alert("Adicionado 10% de desconto!!\nO total deste cliente é: R$" + pagamento.toFixed(2));
-            break;
-        } else if(metodo === 2) { 
-            let pagamento = valorConta / numeroPessoas;
-            alert("O total deste cliente é: R$" + pagamento.toFixed(2));
-            break;
-        }
-    }
-}
-*/
-
 function calcularPagamento() {
     // Recupera os valores dos campos do formulário
     const valorConta = parseFloat(document.getElementById("valor").value);
