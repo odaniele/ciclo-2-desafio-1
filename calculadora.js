010d4282b97698c3d035fd1c40e238c3f441c962
Daniele Oliveira <daniele.oliveiras@hotmail.com>
2024-04-17 19:54:47 -0300

    atualizacao calculo

--- calculadora.js
+++ calculadora.js
@@ -1,27 +1,32 @@
-function calcularPagamento() {
+function calcularPagamento() {   
     
-    let valorConta = parseFloat(document.getElementById("valor").value);
-    let numeroPessoas = parseInt(document.getElementById("pessoas").value);
-    let metodo = parseInt(document.getElementById("metodo").value);
-    let gorjeta = document.getElementById("gorjeta").checked ? valorConta * 0.1 : 0;
-    let pagamentoCliente = valorConta / numeroPessoas;
+let valorConta = parseFloat(document.getElementById("valor").value);
+let numeroPessoas = parseInt(document.getElementById("pessoas").value);
+let metodo = parseInt(document.getElementById("metodo").value);
+let temGorjeta = document.getElementById("gorjeta").checked 
+let pagamentoCliente = valorConta / numeroPessoas;
+let desconto = (pagamentoCliente/100) * 10;
 
-    if (gorjeta > 0) {
-        pagamentoCliente += gorjeta;
-    }
+let calculoGorjeta = pagamentoCliente/100 * 10;
 
-    while(true){
-        console.log("Método de pagamento:");
-        if(metodo === 1) {
-            let desconto = (pagamentoCliente / 100) * 10;
-            let pagamento = pagamentoCliente - desconto;
-            alert("Adicionado 10% de desconto!!\nO total deste cliente é: R$" + pagamento.toFixed(2));
-            break;
-        } else if(metodo === 2) { 
+    if(metodo === 1 && temGorjeta){    
+            let pagamento = (pagamentoCliente - desconto);   
+            let pagamentoDesconto = (pagamento + calculoGorjeta); 
+            alert("Adicionado desconto de 10% no valor de: R$" + desconto.toFixed(2));
+            alert("O total deste cliente é: R$" + pagamentoDesconto.toFixed(2));
+            
+        }else if(metodo === 1 && !temGorjeta){
+            let pagamentoSemGorjeta = (pagamentoCliente - desconto);
+            alert("Adicionado desconto de 10% no valor de: R$" + desconto.toFixed(2));
+            alert("O total deste cliente é: R$" + pagamentoSemGorjeta.toFixed(2));
+
+        }else if(metodo === 2 && temGorjeta){ 
+            let pagamentoSemDesconto = (pagamentoCliente + calculoGorjeta);
+            alert("O total deste cliente é: R$" + pagamentoSemDesconto.toFixed(2));
+        
+        }else{
             alert("O total deste cliente é: R$" + pagamentoCliente.toFixed(2));
-            break;
-        }
-    }
-}
-
-calcularPagamento();
+            }
+        } 
+            
+    calcularPagamento();
